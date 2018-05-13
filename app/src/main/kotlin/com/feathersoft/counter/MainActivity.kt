d6cc321b8827d56fbb6d9655695fb368c9981ee0
Jorge Mendez <grzegorz.feathers@gmail.com>
2018-05-13 14:11:43 -0700

    Adds Add action button

--- app/src/main/kotlin/com/feathersoft/counter/MainActivity.kt
+++ app/src/main/kotlin/com/feathersoft/counter/MainActivity.kt
@@ -2,10 +2,22 @@ package com.feathersoft.counter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+  lateinit var homeContainer: View
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    homeContainer = findViewById(R.id.home_container)
+
+    findViewById<FloatingActionButton>(R.id.home_button_add).setOnClickListener {
+      Snackbar.make(homeContainer, "Add", Snackbar.LENGTH_SHORT).show()
+    }
   }
 }
---
+++ app/src/main/kotlin/com/feathersoft/counter/model/Counter.kt
@@ -0,0 +1,3 @@
+package com.feathersoft.counter.model
+
+data class Counter(val name: String)
